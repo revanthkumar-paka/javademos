24f3eeda204204b4f8e1abbd92927765ed6b9852
unknown <revanthpaka@gmail.com>
2022-03-28 12:34:12 +0530

    this fix

---
+++ src/helloworld.java
@@ -0,0 +1,19 @@
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		byte xbyte=10;
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		int l=1;
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+	    float f=Float.MAX_VALUE;
+	    float f1=Float.MIN_VALUE;
+	    System.out.println(f);
+	    System.out.println(f1);
+	    double f2=5d/2d;    
+	    System.out.println(f);
+	}
+}
